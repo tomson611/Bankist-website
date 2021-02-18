989a4413cb7f34f7f9bf589703ee8ec5f75d7e4c
Tomson <twesolowski61@gmail.com>
2021-02-18 18:17:04 +0100

    selecting elements practice

--- script.js
+++ script.js
@@ -29,3 +29,19 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////////////////
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+document.getElementsByClassName('btn');
